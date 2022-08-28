9544f931f10a622d1fc4704d621093f57a74f56b
GitHub Action <action@github.com>
2022-08-28 14:05:54 +0000

    Template cleanup

--- src/main/kotlin/com/github/niubiliter/jsonzip/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/niubiliter/jsonzip/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.niubiliter.jsonzip.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.niubiliter.jsonzip.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
